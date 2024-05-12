45a0f3569ffa2182594ede4a1685e7b78e4c3264
Sauravr2020 <110717296+Sauravr2020@users.noreply.github.com>
2024-05-12 08:01:58 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,19 +4,24 @@ function Footer() {
       <div>
         <span className="text-xl text-indigo-900 font-bold">FAQ</span>
       </div>
-      <div className="border border-indigo-900 rounded-md p-1 mt-2 ml-2 flex justify-between items-center">
+      <div className="border border-indigo-900 rounded-md p-1 mt-2 ml-2 mb-2 flex justify-between items-center">
         <span>Can education flashcard be used for all age groups?</span>
         <select name="city"></select>
       </div>
-      <div className="border border-indigo-900 rounded-md p-1 mt-2 ml-2 flex justify-between items-center">
+      <div className="border border-indigo-900 rounded-md p-1 mt-2 ml-2 mb-2 flex justify-between items-center">
         <span>How do education flashcards work?</span>
         <select name="city"></select>
       </div>
-      <div className="border border-indigo-900 rounded-md p-1 mt-2 ml-2 flex justify-between items-center">
+      <div className="border border-indigo-900 rounded-md p-1 mt-2 ml-2 mb-2 flex justify-between items-center">
         <span>Can education flashcard be used test preparation?</span>
         <select name="city"></select>
       </div>
     </footer>
+  );
+}
+
+export default Footer;
+
   );
 }
 
